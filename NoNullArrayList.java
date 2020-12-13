d68a5fc5d0ed6cc1f333905e23a57296ccb6a868
Justin Dylan <justin82049@gmail.com>
2020-12-12 23:12:48 -0500

    initialcapacity constructor

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -4,6 +4,9 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(){
     super();
   }
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
   public boolean add(T element){
     if (element == null){
       throw new IllegalArgumentException("bad");
