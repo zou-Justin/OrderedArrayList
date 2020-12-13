529128245741a2304601b735fd515b12f575fe83
Justin Dylan <justin82049@gmail.com>
2020-12-13 15:51:59 -0500

    general gist of boolean add method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -3,6 +3,13 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(){
     super();
   }
+  public boolean add(T element){
+    for (int i = 0; i < this.size();i++){
+      this.get(i).compareTo(element);
+    }
+    super.add();
+  }
 
+]
 
 }
