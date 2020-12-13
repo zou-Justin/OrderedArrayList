43acce29803d11db77fb814a470be629f0376d23
Justin Dylan <justin82049@gmail.com>
2020-12-12 20:05:17 -0500

    added the new add boolean method

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,2 +1,11 @@
 import java.util.IllegalArgumentException;
-public class NoNullArrayList<T> extends ArrayList<T>
+public class NoNullArrayList<T> extends ArrayList<T>{
+
+  public boolean add(T element){
+    if (element == null){
+      throw IllegalArgumentException("bad");
+    }
+    Super.add(element);
+  }
+
+}
